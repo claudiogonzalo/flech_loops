f30003362dfc2dac061552e980cd30f049ae153e
claudio gonzalo <gonzaloclaudio@gmail.com>
2021-08-20 16:09:59 -0400

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,29 @@
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 1; index++) {
     music.playTone(220, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.North)
     music.playTone(262, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.NorthEast)
     music.playTone(311, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.East)
     music.playTone(330, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.SouthEast)
     music.playTone(220, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.South)
     music.playTone(262, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.SouthWest)
     music.playTone(311, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.West)
     music.playTone(659, music.beat(BeatFraction.Sixteenth))
-    //  loops
+    // loops
     basic.showArrow(ArrowNames.NorthWest)
 }
+music.setVolume(45)
+music.playMelody("D - F - A - G - ", 289)
+soundExpression.happy.playUntilDone()
